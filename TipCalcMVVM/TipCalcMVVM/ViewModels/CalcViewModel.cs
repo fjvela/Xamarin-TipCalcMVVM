18bbb73e198080425473b76b823f8486c5bfb6ae
javier.vela <javier.vela@SF018198>
2017-03-12 20:28:58 +0100

    Add project files.

---
+++ TipCalcMVVM/TipCalcMVVM/ViewModels/CalcViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TipCalcMVVM.ViewModels
+{
+    public class CalcViewModel : BindableObject
+    {
+        private decimal subTotal;
+        private decimal tip;
+        private decimal tipValue;
+        private decimal total;
+        private decimal totalAfterTax;
+
+        public CalcViewModel()
+        {
+        }
+
+        public ICommand CalculateTipCommand { get { return new Command(CalculateTip); } }
+        public decimal SubTotal
+        {
+            get { return subTotal; }
+            set
+            {
+                subTotal = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public decimal Tip
+        {
+            get { return tip; }
+            set
+            {
+                tip = value;
+                OnPropertyChanged();
+            }
+        }
+
+        
+
+        public decimal TipValue
+        {
+            get { return tipValue; }
+            set { tipValue = value; OnPropertyChanged(); }
+        }
+
+        public decimal Total
+        {
+            get { return total; }
+            set { total = value; OnPropertyChanged(); }
+        }
+
+        public decimal TotalAfterTax
+        {
+            get { return totalAfterTax; }
+            set
+            {
+                totalAfterTax = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void CalculateTip()
+        {
+            TipValue = System.Math.Round((SubTotal * Tip) / 100, 2);
+            Total = TotalAfterTax + TipValue;
+        }
+
+     
+    }
+}
---
+++ TipCalcMVVM/TipCalcMVVM/Views/CalcPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TipCalcMVVM.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TipCalcMVVM.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalcPage : ContentPage
+    {
+        public CalcPage()
+        {
+            InitializeComponent();
+            BindingContext = new CalcViewModel();
+            
+        }
+    }
+}
